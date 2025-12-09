37ccba850d175c1959b0a77e441a65db1b1257bc
Ezkermish <carlosa.quiroz@gmail.com>
2025-12-09 04:03:23 -0500

    Recupera datos en FrontEnd

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import useFetch from './UseFetch';
+import useFetch from './UseFetch'
+import './FetchData.css'
 
 const FetchData = () => {
 const [data]=useFetch('https://api.npoint.io/9045c260b1565daa9e15');
 console.log(data);
   return (
     <>
+    <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
      <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+        {data && data.map((e)=>(
+            <li key={index} className='list_data'>
+                <h3>{e.name}</h3>
+                <p><strong>Importance: </strong>{e.importance}</p>
+                <p><strong>Benefits: </strong>{e.benefits}</p>
+                <p><strong>Tome to eat: </strong>{e.best_time_to_intake}</p>
+            </li>
+        ))}
      </ul>
     </>
   )
