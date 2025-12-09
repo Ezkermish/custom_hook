771ac40adecf132371a47632f32ac3bee3558839
Ezkermish <carlosa.quiroz@gmail.com>
2025-12-09 04:08:00 -0500

    Recupera datos en FrontEnd

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -9,7 +9,7 @@ console.log(data);
     <>
     <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
      <ul className='list_data_main'>
-        {data && data.map((e)=>(
+        {data && data.map((e,index)=>(
             <li key={index} className='list_data'>
                 <h3>{e.name}</h3>
                 <p><strong>Importance: </strong>{e.importance}</p>
